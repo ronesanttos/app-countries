421aba8bd6886a046fe798308d7a8d58821470d5
ronesanttos <devronesantos@gmail>
2024-11-27 21:25:30 -0300

    corrigindo erros

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
+  resolve: {
+    alias: {
+      'react-router-dom': require.resolve('react-router-dom'), // Garante que o Vite resolva corretamente o pacote
+    },
+  },
 })
